fdc88094edeb35b6c0b6376974f9438a06120278
Adam Wolff <awolff@gmail.com>
2020-09-27 14:21:25 -0700

    multi-select (whew!)

--- js/src/Roster.js
+++ js/src/Roster.js
@@ -1,36 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import sample_figure from './figure.jpg';
-import MultiSelect from "react-multi-select-component";
 import { v4 as uuidv4 } from 'uuid';
-
-const Weapons = () => {
-  const options = [
-    { label: "Grapes 🍇", value: "grapes" },
-    { label: "Mango 🥭", value: "mango" },
-    { label: "Watermelon 🍉", value: "watermelon" },
-    { label: "Pear 🍐", value: "pear" },
-    { label: "Apple 🍎", value: "apple" },
-    { label: "Tangerine 🍊", value: "tangerine" },
-    { label: "Pineapple 🍍", value: "pineapple" },
-    { label: "Peach 🍑", value: "peach" },
-  ];
- 
-  const [selected, setSelected] = useState([]);
- 
-  return (
-    <MultiSelect
-    options={options}
-    value={selected}
-    onChange={setSelected}
-    labelledBy={"Select"}
-    />
-  );
-}
 
 function Character(props) {
     const c = props.info;
     const name = "name" in props.info ? props.info.name : "New Character";
     const changeName = e => props.updateInfo({...c, name: e.target.value});
+    const setProfile = e => props.updateInfo({...c, profile: e.target.value});
+    const weapons = "weapons" in c ? c.weapons : [];
+    console.log("weaps", weapons)
+    const setWeapons = e => props.updateInfo(
+            {...c, weapons: [...e.target.selectedOptions].map(o => o.value)});
     return (
         <form><div className="pure-g">
             <div className="pure-u-1-2">
@@ -44,19 +24,28 @@ function Character(props) {
                 <label htmlFor="profile">
                     Profile:
                 </label>
-                <select name="profile" id="profile">{
+                <select name="profile" value={c.profile} onChange={setProfile}>{
                     props.profiles.map(p => (
-                        <option value={p.Name} key={p.Name}>{p.Name}</option>
+                        <option value={p.id} key={p.id}>{p.Name}</option>
                     ))}
                 </select>
                 </div>
-                <label>
+                <div>
+                <label htmlFor="weapons">
                     Weapons:
                 </label>
-                <Weapons />
+                <select name="weapons" value={weapons} multiple={true}
+                        onChange={setWeapons}>{
+                    props.weapons.map(p => (
+                        <option value={p.id} key={p.id}>{p.name}</option>
+                    ))}
+                </select>
+                </div>
+                <div>
                 <button type="button" onClick={_ => props.remove(c.id)}>
-                    <span role="img" aria-label="x">❎</span>
+                    Remove <span role="img" aria-label="x">❎</span>
                 </button>
+                </div>
             </div>
         </div></form>
     );
@@ -67,18 +56,18 @@ function Roster(props) {
         props.setRoster(props.roster.concat({id: uuidv4()}));
         e.preventDefault();
     }
-    console.log('up', props.roster[0])
     const updateInfo = info => 
         props.setRoster(props.roster.map(c => c.id === info.id ? info : c));
     const remove = id => 
         props.setRoster(props.roster.filter(c => c.id !== id));
+    const weapons = [{id: 1, name: "foo"}, {id:3, name:"bar"}];
 
     return (<>
         <div><form onSubmit={addCharacter}>
             <button type="submit">Add</button>
         </form></div>{
         props.roster.map(c => 
-            <Character info={c} key={c.id} profiles={props.profiles}
+            <Character info={c} key={c.id} profiles={props.profiles} weapons={weapons}
                        updateInfo={updateInfo} remove={remove}/>)
         }
     </>);
